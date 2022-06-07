e03d4bb003c4bf2244e06229eb12d7bc3bddd9b5
perilEcho <69120418+perilEcho@users.noreply.github.com>
2022-06-07 12:03:02 -0700

    Updated rotated array

--- 301/rotated_array.py
+++ 301/rotated_array.py
@@ -1,11 +1,11 @@
 """
-[STATUS: WIP]
+[STATUS: SOLVED]
 echo4eva
 rotated_array.py
 6/7/2022
 
 [PCing UMPIRE Video]
-https://youtu.be/0LEmrlcicX0
+https://youtu.be/0LEmrlcicX0 (wrong solution but still good communicating)
 
 [PLAN]
 1. Get user input of the list
@@ -13,17 +13,15 @@ https://youtu.be/0LEmrlcicX0
 3. Binary Search for the most minimum index
     - Just in case there are duplicates
 4. Return the minimum index
-"""
 
 def findMin(arr):
     minValue = min(arr)
-    minIndex = -1
 
     left =  0
     right = len(arr) - 1
 
     while left <= right:
-        mid = left + right // 2
+        mid = (left + right) // 2
 
         if arr[mid] == minValue:
             minIndex = mid
@@ -34,6 +32,26 @@ def findMin(arr):
 
         else:
             right = mid - 1
+    
+    return minIndex
+
+[PLAN 2]
+1. Get user input of list
+2. Find out min value is
+3. Get the index of min value
+4. Return the index value
+"""
+
+def findMin(arr):
+    counter = 0
+    minValue = arr[0]
+    minIndex = -1
+
+    while counter < len(arr):
+        if arr[counter] < minValue:
+            minValue = arr[counter]
+            minIndex = counter
+        counter += 1
     
     return minIndex
 
